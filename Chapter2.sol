d49f258c7318e9f7e4fc7871ec2855ac5a84ab87
mixed_nuts_nuts <88083304+mixnuts07@users.noreply.github.com>
2022-05-17 20:26:43 +0900

    Update Chapter2.sol

--- Chapter2.sol
+++ Chapter2.sol
@@ -29,6 +29,31 @@ contract BabyDoge is Doge{
 ・変数の格納場所 .. storage, memory
 storage(HDD) ..チェーン上に永久に格納される変数。状態変数(関数外で宣言された変数)。
 memory(RAM) .. 一時的な変数。外部関数をcontractにcallするときに消去される。(関数内で宣言された変数)
+EX...
+contract SandwichFactory {
+  struct Sandwich {
+    string name;
+    string status;
+  }
 
+  Sandwich[] sandwiches;
+
+  function eatSandwich(uint _index) public {
+    // 明示的に宣言！
+    
+    // storage宣言：
+    Sandwich storage mySandwich = sandwiches[_index];
+    //この場合`mySandwich`がstorage内の`sandwiches[_index]`を示すポインタだから...
+    mySandwich.status = "Eaten!";
+    // これで sandwiches[_index] をブロックチェーン上でも永久に変更することになる。
+
+    // コピーしたいだけなら memory の方が便利：
+    Sandwich memory anotherSandwich = sandwiches[_index + 1];
+    // この場合 anotherSandwich は memory内のデータをコピーすることになり
+    anotherSandwich.status = "Eaten!";
+    // 一時的な変数を変更するだけで sandwiches[_index + 1] にはなんの影響もない。次のようにすることも可能 
+    sandwiches[_index + 1] = anotherSandwich;
+  }
+}
 
 
