3c87ebd3ebb0b45ef396de617656605ad1fe927d
mixed_nuts_nuts <88083304+mixnuts07@users.noreply.github.com>
2022-05-17 14:53:15 +0900

    Update Chapter2.sol

--- Chapter2.sol
+++ Chapter2.sol
@@ -6,4 +6,11 @@ ETHのブロックチェーンはアカウント(口座)で構成されている
 mapping(address => uint) public accountBalance; 
 // ユーザIDをもとにユーザ名を参照・格納 key=uint, value=string
 mapping(uint=> string) useIdToName;
-・msg.sender...ユーザのaddressを参照できるG変数。
+・msg.sender...ユーザのaddressを参照できるG変数。関数をcallするときに使用。securityをもたらす。
+・require ... 条件を満たさないとエラーを投げて実行STOP.
+// require(aa == ??);
+   return hoge;
+・solidityはネイティブで文字列比較ができない。→keccak256でハッシュを比較!! 
+Ex.. require( keccak256(_name) == keccak256("Vital") );
+
+
